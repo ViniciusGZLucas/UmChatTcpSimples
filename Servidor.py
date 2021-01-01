f6ec38a53e23a5987c76a4ff9eeefedbe5bc12e5
VicnorV2 <viniciusgabriel.z.lucas@gmail.com>
2021-01-01 00:47:51 -0300

    Update Servidor.py

--- Servidor.py
+++ Servidor.py
@@ -2,16 +2,19 @@ import socket
 import threading
 import time
 
-Host = "192.168.86.3"
-Port = 11111
+Host = "192.168.86.3"#Ip da maquina que ira rodar o servidor
+Port = 11111#Porta desejada
 
+#Cria o socket -> Seta para poder receber informações do ip e porta escolhida -> Fica acessível para pessoas conectarem.
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.bind((Host,Port))
 s.listen()
 
+#Variaveis que irão salvar as conexões ativas e nicks
 Conexoes = []
 Nomes = []
 
+#Função que ira enviar mensagem para as pessoas conectadas.
 def Mandar(Mensagem):
     for Conexao in Conexoes:
         try:
@@ -19,6 +22,7 @@ def Mandar(Mensagem):
         except:
             pass
 
+#Função que Ira criar um thread para cada pessoa conectada e ira mostrar no console o ip/porta e nick das pessoas que conectarem
 def Start():
     while True:
         Conn,Address = s.accept()
@@ -31,6 +35,8 @@ def Start():
         Thread = threading.Thread(target=Receber,args=(Conn,))
         Thread.start()
 
+#Função responsavel por ficar verificando se o usuario que ela estiver responsavel mandou alguma mensagem para repassar para os outros
+#ou fechar a conexão com alguma pessoa que saia do chat.
 def Receber(Conn):
     while True:
         try:
